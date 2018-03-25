217eb96a08f3d5dff7ca805226e476655945b3cb
EC2 Default User <ec2-user@ip-10-88-170-119.ap-southeast-2.compute.internal>
2018-03-25 02:58:22 +0000

    First commit

---
+++ dbload.java
@@ -0,0 +1,88 @@
+import java.io.*;
+
+public class dbload {
+
+	public static void main(String[] args) {
+        int pagesize = 0;
+        String datafile = null;
+        try{
+            pagesize = Integer.parseInt(args[1]);
+            datafile = args[2];    
+        }
+        catch(Exception e){
+            System.out.println("Invalid arguments. Please try again");
+            System.exit(0);
+        }
+		// TODO Auto-generated method stub
+		long startTime, endTime, duration;
+		
+		startTime = System.currentTimeMillis();
+		heapFile(pagesize, datafile);
+		endTime = System.currentTimeMillis();
+		duration = (endTime - startTime);
+		System.out.println("Time taken for creating heapfile page size " + pagesize + ": " + (duration/1000) + "s");
+	}
+
+	public static void heapFile(int pagesize, String datafile){
+		File file = new File("heap" + "." + pagesize);
+		int pageNum = 1;
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(datafile));
+			PrintWriter pw = new PrintWriter(new FileWriter(file));
+			
+			String in;
+			int page = 0;
+			String[] header = (br.readLine()).split("\t"); //read pass the header
+			
+            pw.println("page 0");
+			while((in = br.readLine()) != null){
+                String data = new String();
+                
+                //split the line, change the id
+                String[] token = in.split("\t");
+                
+                for(int i=1; i<header.length; i++){
+                    try{
+                        data = data.concat(header[i] + ":" + token[i]);
+                    }
+                    catch(IndexOutOfBoundsException ie){
+                        data = data.concat(header[i] + ":" + "N/A");
+                    }
+                    
+                    if(i == header.length-1){
+                        data = data.concat("\r\n");
+                    }
+                    else{
+                        data = data.concat("\t");
+                    }
+                }
+                byte[] b = data.getBytes();
+                
+				if((page + b.length) < pagesize){
+                    pw.print(data);
+					page += b.length;
+				}
+				else{
+                    System.out.print("Writing page " + pageNum + ". Previous page size is " + page + "\r");
+                    pw.println("page " + pageNum);
+					page = b.length;
+					pageNum++;
+				}
+			}
+			br.close();
+			pw.close();
+            
+            System.out.println();
+            System.out.println("Finish loading.");
+            System.out.println("Total page : " + pageNum);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ dbquery.java
@@ -0,0 +1,80 @@
+import java.io.*;
+
+public class dbquery {
+
+	public static void main(String[] args) {
+        String query = null;
+        String pagesize = null;
+        try{
+            query = args[0];
+            pagesize = args[1];
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            System.out.println("Invalid arguments. Please try again");
+            System.exit(0);
+        }        
+		// TODO Auto-generated method stub
+		long startTime, endTime, duration;
+		
+		startTime = System.currentTimeMillis();
+		searchText(query, pagesize);
+		endTime = System.currentTimeMillis();
+		duration = (endTime - startTime);
+		System.out.println("Time taken for search : " + (duration/1000) + "s");
+	}
+
+	public static void searchText(String query, String pagesize){
+		File file = new File("heap" + "." + pagesize);
+        
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			
+			String in;
+			int page = -1;
+            int count = 0;
+			
+			while((in = br.readLine()) != null){
+                //read page
+                if(Character.toString(in.charAt(0)).equals("p")){
+                    page++;
+                    System.out.print("Searching in page " + page + "...\r");
+                }
+                else{
+                    String[] token = in.split("\t");
+                    
+                    for(int i=0; i<token.length; i++){
+                        //split the datainto 2 part
+                        String[] data = token[i].split(":",2);
+                        
+                        //at this point, only search in BN_NAME
+                        if(data[0].equals("BN_NAME")){
+                            String check1 = query.toLowerCase();
+                            String check2 = data[1].toLowerCase();
+                            
+                            if(check2.contains(check1)){
+                                System.out.print("Found '" + query + "' in page " + page + ": ");
+                                System.out.print(data[1] + "\r\n");
+                                count++;
+                            }
+                        }
+                    }
+                }
+			}
+			br.close();
+            
+            System.out.println();
+            System.out.println("Finish loading.");
+            System.out.println(count + " result(s) found");
+            
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+    
+}
